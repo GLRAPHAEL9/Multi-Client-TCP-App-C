0a4bf907e6cafa218da7e720c45e84afb7edbf5f
Gabriel Raphael <gabriel.raphael1@outlook.com>
2025-08-20 08:55:43 +0100

    Initialised Winsock. Setup of the server address.

--- client_gabriel.c
+++ client_gabriel.c
@@ -30,3 +30,45 @@ void receive_messages(void *arg) {
     }
     _endthread();
 }
+
+int main() {
+    WSADATA wsa;
+    struct sockaddr_in server_addr;
+
+    // Initialize Winsock
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    // Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        printf("Socket error\n");
+        return 1;
+    }
+
+    // Setup server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Localhost
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        printf("Connection failed\n");
+        return 1;
+    }
+
+    // Send username to server
+    send(sock, username, strlen(username), 0);
+
+    // Start receiving messages in background thread
+    _beginthread(receive_messages, 0, NULL);
+
+    // Main loop: read user input and send to server
+    char buffer[BUFFER_SIZE];
+    while (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
+        send(sock, buffer, strlen(buffer), 0);
+    }
+
+    closesocket(sock);
+    WSACleanup();
+    return 0;
+}
